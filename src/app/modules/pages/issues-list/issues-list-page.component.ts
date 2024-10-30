3f57723785fa8029b5f36b4069428fc92d725764
Jhonny <jhonnycgarcia@gmail.com>
2024-10-30 11:24:53 -0500

    Mostrar labels en pantalla

--- src/app/modules/pages/issues-list/issues-list-page.component.ts
+++ src/app/modules/pages/issues-list/issues-list-page.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IssuesService } from '../../services/issues.service';
+import { LabelsSelectorComponent } from "../../components/labels-selector/labels-selector.component";
 
 @Component({
   selector: 'app-issues-list-page',
@@ -9,7 +10,8 @@ import { IssuesService } from '../../services/issues.service';
   imports: [
     CommonModule,
     RouterLink,
-  ],
+    LabelsSelectorComponent
+],
   templateUrl: './issues-list-page.component.html'
 })
 export default class IssuesListPageComponent {
