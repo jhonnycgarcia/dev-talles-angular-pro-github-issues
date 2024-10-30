9a2aabe3ce01738506e7f35d06414653f488727e
Jhonny <jhonnycgarcia@gmail.com>
2024-10-30 18:50:23 -0500

    Pruebas - Debe de cargar labels

--- src/app/modules/services/issue.service.spec.ts
+++ src/app/modules/services/issue.service.spec.ts
@@ -1,9 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { IssueService } from './issue.service';
 import { provideAngularQuery, QueryClient } from '@tanstack/angular-query-experimental';
+import { IssuesService } from './issues.service';
 
-describe('IssueService', () => {
-  let service: IssueService;
+describe('IssuesService', () => {
+  let service: IssuesService;
   const queryClient = new QueryClient();
 
   beforeEach(() => {
@@ -11,10 +11,27 @@ describe('IssueService', () => {
       teardown: { destroyAfterEach: false },
       providers: [ provideAngularQuery(queryClient) ]
     });
-    service = TestBed.inject(IssueService);
+    service = TestBed.inject(IssuesService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should load labels', async () => {
+    const { data } = await service.labelsQuery.refetch();
+    expect(data?.length).toBe(30);
+
+    const [label] = data!;
+
+    expect(typeof label.id).toBe('number');
+    expect(typeof label.node_id).toBe('string');
+    expect(typeof label.url).toBe('string');
+    expect(typeof label.name).toBe('string');
+    expect(typeof label.color).toBe('string');
+    expect(typeof label.default).toBe('boolean');
+    expect(typeof label.description).toBe('string');
+
+  });
+
 });
